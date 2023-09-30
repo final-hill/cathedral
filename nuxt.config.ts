702056b7fbf5ebfbe36ac6d9a239494b073dcb0e
Michael Haufe <tno@thenewobjective.com>
2023-09-30 09:53:05 -0500

    global nav and icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
+import { defineNuxtConfig } from 'nuxt/config'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    css: ['~/assets/css/main.css'],
     devtools: { enabled: true },
     ssr: false,
-    spaLoadingTemplate: 'public/_loading-template.html'
+    spaLoadingTemplate: 'public/_loading-template.html',
+    modules: [
+        // https://nuxt.com/modules/icon
+        // https://icones.js.org/
+        'nuxt-icon'
+    ]
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
     <div>
-        <h2>Index</h2>
+        <h2>Home</h2>
     </div>
 </template>
