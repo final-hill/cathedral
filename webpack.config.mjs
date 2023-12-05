823b93603a20ac2badb0ab14464e897d26b62948
Michael Haufe <tno@thenewobjective.com>
2023-12-05 13:14:40 -0600

    25 define pwa (#41)
    * Updated icon manifest
    
    * Reduced image size

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -2,20 +2,21 @@ import path from 'path';
 import url from 'url';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import ESLintWebpackPlugin from 'eslint-webpack-plugin';
+import FaviconsWebpackPlugin from 'favicons-webpack-plugin';
 import { GenerateSW } from 'workbox-webpack-plugin';
+import pkg from './package.json' with { type: "json" }
 
 const __filename = url.fileURLToPath(import.meta.url),
     __dirname = path.dirname(__filename);
 
 export default {
-    devtool: 'source-map',
+    mode: 'production',
     entry: {
         main: './src/main.mts'
     },
     experiments: {
         outputModule: true
     },
-    mode: 'production',
     module: {
         rules: [
             { test: /\.([cm]?ts|tsx)$/, loader: "ts-loader" },
@@ -62,11 +63,45 @@ export default {
             fix: true,
             overrideConfigFile: path.resolve(__dirname, '.eslintrc.json')
         }),
+        new FaviconsWebpackPlugin({
+            logo: './src/presentation/assets/images/logo.png',
+            logoMaskable: './src/presentation/assets/images/logo-maskable.png',
+            cache: true,
+            favicons: {
+                appName: 'Cathedral',
+                appDescription: pkg.description,
+                developerName: pkg.author,
+                developerURL: pkg.homepage,
+                display: 'standalone',
+                theme_color: '#212830',
+                background: '#2d3843',
+                lang: 'en-US',
+                icons: {
+                    coast: false,
+                    yandex: false
+                }
+            }
+        }),
         new HtmlWebpackPlugin({
             title: 'Cathedral',
-            favicon: './src/presentation/assets/icons/favicon.ico',
             scriptLoading: 'module',
-            publicPath: '/'
+            publicPath: '/',
+            templateContent: ({ htmlWebpackPlugin }) => `
+                <!DOCTYPE html>
+                <html lang="en" dir='ltr'>
+                    <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1">
+                        <title>${htmlWebpackPlugin.options.title}</title>
+                        <meta name="description" content="${pkg.description}">
+                    </head>
+                    <body>
+                        Loading...
+                        <noscript>
+                            This application requires JavaScript to be enabled.
+                        </noscript>
+                    </body>
+                </html>`
         }),
         new GenerateSW({
             navigateFallback: 'index.html',
