bcd725d92009ec2e516d98e4e839bb1c218c7899
Michael L Haufe <tno@thenewobjective.com>
2024-06-18 18:01:53 +0000

    PGLite Hello World

---
+++ pages/hello-pglite.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { PGlite } from "@electric-sql/pglite";
+
+const db = new PGlite('idb://cathedral'),
+    response = await db.query("select 'Hello world' as message;");
+</script>
+
+<template>
+    <pre>{{ JSON.stringify(response) }}</pre>
+</template>
