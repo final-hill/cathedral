95b26a269beed3f01f5a9cdab11b9f1a46b4d16d
Michael L Haufe <tno@thenewobjective.com>
2024-06-28 15:23:23 +0000

    Implemented Functionality entity and associated table

--- domain/NonFunctionalBehavior.ts
+++ domain/NonFunctionalBehavior.ts
@@ -1,3 +1,8 @@
-import Behavior from "./Behavior";
+import Functionality from "./Functionality";
 
-export default class NonFunctionalBehavior extends Behavior { }
+/**
+ * NonFunctionalBehavior is a type of Behavior that is not directly related to the functionality of a system.
+ * It specifies how the system should behave, i.e., the qualities that the system must exhibit.
+ * Generally expressed in the form "system shall be <requirement>."
+ */
+export default class NonFunctionalBehavior extends Functionality { }
