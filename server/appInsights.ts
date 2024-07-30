e73b1b28c268d484ed399b1bf21ad2942694934f
Michael Haufe <tno@thenewobjective.com>
2024-07-29 19:45:13 -0500

    Added Application Insights library (#291)

---
+++ server/appInsights.ts
@@ -0,0 +1,13 @@
+import * as appInsights from 'applicationinsights';
+
+appInsights.setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY)
+    .setAutoCollectRequests(true)
+    .setAutoCollectPerformance(true, true)
+    .setAutoCollectExceptions(true)
+    .setAutoCollectDependencies(true)
+    .setAutoCollectConsole(true, true)
+    .setAutoCollectPreAggregatedMetrics(true)
+    .setSendLiveMetrics(false)
+    .setInternalLogging(false, true)
+    .enableWebInstrumentation(false)
+    .start();
