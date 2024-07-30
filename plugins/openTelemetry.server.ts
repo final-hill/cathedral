d69fdd71773237436ce40d5cfb4580322786183f
Michael Haufe <tno@thenewobjective.com>
2024-07-30 14:37:40 -0500

    swapped applicationInsights for openTelemetry (#297)

---
+++ plugins/openTelemetry.server.ts
@@ -0,0 +1,22 @@
+// ref: https://learn.microsoft.com/en-us/azure/azure-monitor/app/opentelemetry-enable?tabs=nodejs
+import { useAzureMonitor, type AzureMonitorOpenTelemetryOptions } from "@azure/monitor-opentelemetry";
+
+export default defineNuxtPlugin(async nuxtApp => {
+    if (!process.env.APPLICATIONINSIGHTS_CONNECTION_STRING)
+        return;
+
+    // https://www.npmjs.com/package/@azure/monitor-opentelemetry
+    const options: AzureMonitorOpenTelemetryOptions = {
+        samplingRatio: 1,
+        azureMonitorExporterOptions: {
+            connectionString: process.env.APPLICATIONINSIGHTS_CONNECTION_STRING
+        },
+        instrumentationOptions: {
+            http: { enabled: true }
+        },
+        enableLiveMetrics: true,
+        enableStandardMetrics: true,
+    }
+
+    useAzureMonitor(options);
+})
