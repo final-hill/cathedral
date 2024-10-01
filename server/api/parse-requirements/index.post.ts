bbaafdc603e11c22537f789f1e7732e26c77931c
Michael Haufe <tno@thenewobjective.com>
2024-10-01 17:46:18 -0500

    - Fixed env bug (#372)
    - Added Object.groupBy polyfill

--- server/api/parse-requirements/index.post.ts
+++ server/api/parse-requirements/index.post.ts
@@ -71,6 +71,19 @@ export default defineEventHandler(async (event) => {
         [K in ArrayToUnion<typeof result>['type']]?: ExtractGroupItem<K>[]
     }
 
+    // The server is currently Node 20 which does not support Object.groupBy.
+    // See: https://github.com/final-hill/cathedral/issues/371
+    if (!Object.groupBy) {
+        Object.groupBy = function <K extends PropertyKey, T>(items: Iterable<T>, keySelector: (item: T, index: number) => K): Partial<Record<K, T[]>> {
+            return [...items].reduce((acc, item, index) => {
+                const key = keySelector(item, index),
+                    group = (acc as any)[key as any] ?? ((acc as any)[key as any] = [])
+                group.push(item)
+                return acc
+            }, {} as Partial<Record<K, T[]>>)
+        }
+    }
+
     const groupedResult = Object.groupBy(result, ({ type }) => type) as GroupedResult
 
     const parsedRequirement = em.create(ParsedRequirement, {
