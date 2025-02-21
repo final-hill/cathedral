f15f153ac846249704f67997acc49243e4fb993a
Michael Haufe <tno@thenewobjective.com>
2025-02-21 11:19:55 -0600

    Updated dependencies and fixed associated errors (#484)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,8 @@
 import { defineVitestConfig } from '@nuxt/test-utils/config'
+import type { UserConfig } from 'vitest/node'
 
 export default defineVitestConfig({
     test: {
         environment: 'nuxt'
     }
-})
+}) as UserConfig
