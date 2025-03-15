7b20be7d77e8d930ea880a100ad9a89ad4a9cd44
Michael Haufe <tno@thenewobjective.com>
2025-03-15 23:26:08 +0000

    Refactor form state management in XForm component and add success toast notification on submit

--- components/XForm.vue
+++ components/XForm.vue
@@ -18,15 +18,21 @@ const props = defineProps<{
 // Nuxt UI still doesn't support resetting of a form <https://github.com/nuxt/ui/issues/964#issuecomment-1810253480>
 
 const form = useTemplateRef('form'),
-    formState = reactive(Object.create(props.state))
+    backupState = reactive(Object.create(props.state)),
+    toast = useToast()
 
 const onSubmit = ({ data }: FormSubmitEvent<z.infer<F>>) => {
     props.onSubmit(data)
+    toast.add({
+        icon: 'i-lucide-check',
+        title: 'Success',
+        description: 'Data saved successfully'
+    })
 }
 
 const onCancel = () => {
     form.value?.clear()
-    Object.assign(formState, props.state)
+    Object.assign(props.state, backupState)
 
     if (props.onCancel)
         props.onCancel()
@@ -51,41 +57,46 @@ const { data: autocomplete, status: autocompleteStatus } = await useFetch('/api/
 </script>
 
 <template>
-    <UForm ref="form" :id="props.id" :state="formState" :schema="props.schema"
+    <UForm ref="form" :id="props.id" :state="props.state" :schema="props.schema"
         :class="`gap-4 flex flex-col ${props.class}`" @submit="onSubmit" autocomplete="off">
-        <UFormField v-for="field of getSchemaFields(props.schema)" :label="field.label" :name="field.key"
-            :key="field.key" :field="field.key" :required="!field.isOptional" :description="field.description"
-            :hint="field.isReadOnly ? '(Read Only)' : undefined" size="xl" class="w-full"
-            :help="field.innerType instanceof z.ZodString && field.maxLength && field.maxLength > 254 ? `Max length: ${formState[field.key]?.length ?? 0}/${field.maxLength}` : undefined">
+        <template v-for="field of getSchemaFields(props.schema)" :key="field.key">
+            <UInput v-if="field.key === 'id'" type="hidden" v-model="props.state.id" name="id" />
+            <UFormField v-if="field.key !== 'id'" :label="field.label" :name="field.key" :field="field.key"
+                :required="!field.isOptional" :description="field.description"
+                :hint="field.isReadOnly ? '(Read Only)' : undefined" size="xl" class="w-full"
+                :help="field.innerType instanceof z.ZodString && field.maxLength && field.maxLength > 254 ? `Max length: ${props.state[field.key]?.length ?? 0}/${field.maxLength}` : undefined">
 
-            <UCheckbox v-if="field.innerType instanceof z.ZodBoolean" v-model="formState[field.key]" class="w-full" />
-            <UInput type="text"
-                v-else-if="field.innerType instanceof z.ZodString && field.maxLength && field.maxLength <= 254"
-                v-model.trim="formState[field.key]" class="w-full" aria-describedby="character-count"
-                :ui="{ trailing: 'pointer-events-none' }">
-                <template #trailing>
-                    <div id="character-count" class="text-xs text-(--ui-text-muted) tabular-nums" aria-live="polite"
-                        role="status">
-                        {{ formState[field.key]?.length ?? 0 }}/{{ field.maxLength }}
-                    </div>
-                </template>
-            </UInput>
-            <UTextarea v-else-if="field.innerType instanceof z.ZodString && field.maxLength && field.maxLength > 254"
-                v-model.trim="formState[field.key]" class="w-full" autoresize>
-            </UTextarea>
-            <UInput v-else-if="field.isReadOnly" type="text" v-model.trim="formState[field.key]" disabled tabindex="-1"
-                class="w-full" />
-            <UInputNumber v-else-if="field.innerType instanceof z.ZodNumber" v-model.trim="formState[field.key]"
-                :min="field.min" :max="field.max" class="w-full" />
-            <UInput type="datetime-local" v-else-if="field.innerType instanceof z.ZodDate"
-                v-model.trim="formState[field.key]" class="w-full" />
-            <USelect v-else-if="field.isEnum" v-model="formState[field.key]" :items="field.enumOptions"
-                class="w-full" />
-            <UInputMenu v-else-if="field.isObject" v-model="formState[field.key]" :items="autocomplete || []"
-                :loading="autocompleteStatus === 'pending'" class="w-full" />
-            <!-- :loading="(await fetchAutocompleteData(formState[field.key])).status === 'pending'" -->
-            <UInput v-else v-model.trim="formState[field.key]" class="w-full" />
-        </UFormField>
+                <UCheckbox v-if="field.innerType instanceof z.ZodBoolean" v-model="props.state[field.key]"
+                    class="w-full" />
+                <UInput type="text"
+                    v-else-if="field.innerType instanceof z.ZodString && field.maxLength && field.maxLength <= 254"
+                    v-model.trim="props.state[field.key]" class="w-full" aria-describedby="character-count"
+                    :ui="{ trailing: 'pointer-events-none' }">
+                    <template #trailing>
+                        <div id="character-count" class="text-xs text-(--ui-text-muted) tabular-nums" aria-live="polite"
+                            role="status">
+                            {{ props.state[field.key]?.length ?? 0 }}/{{ field.maxLength }}
+                        </div>
+                    </template>
+                </UInput>
+                <UTextarea
+                    v-else-if="field.innerType instanceof z.ZodString && field.maxLength && field.maxLength > 254"
+                    v-model.trim="props.state[field.key]" class="w-full" autoresize>
+                </UTextarea>
+                <UInput v-else-if="field.isReadOnly" type="text" v-model.trim="props.state[field.key]" disabled
+                    tabindex="-1" class="w-full" />
+                <UInputNumber v-else-if="field.innerType instanceof z.ZodNumber" v-model.trim="props.state[field.key]"
+                    :min="field.min" :max="field.max" class="w-full" />
+                <UInput type="datetime-local" v-else-if="field.innerType instanceof z.ZodDate"
+                    v-model.trim="props.state[field.key]" class="w-full" />
+                <USelect v-else-if="field.isEnum" v-model="props.state[field.key]" :items="field.enumOptions"
+                    class="w-full" />
+                <UInputMenu v-else-if="field.isObject" v-model="props.state[field.key]" :items="autocomplete || []"
+                    :loading="autocompleteStatus === 'pending'" class="w-full" />
+                <!-- :loading="(await fetchAutocompleteData(formState[field.key])).status === 'pending'" -->
+                <UInput v-else v-model.trim="props.state[field.key]" class="w-full" />
+            </UFormField>
+        </template>
 
         <div class="flex gap-2">
             <UButton label="Submit" type="submit" color="primary" />
