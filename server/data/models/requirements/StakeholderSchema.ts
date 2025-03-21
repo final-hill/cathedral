fc2713f83ecaebc3ff6df23de2de8cb9710522bf
Michael Haufe <tno@thenewobjective.com>
2025-03-21 17:56:42 +0000

    Refactor VSCode launch configuration and update compatibility date; enhance requirement and stakeholder schemas with validation checks

--- server/data/models/requirements/StakeholderSchema.ts
+++ server/data/models/requirements/StakeholderSchema.ts
@@ -1,4 +1,4 @@
-import { Collection, Entity, Enum, Property, types } from '@mikro-orm/core';
+import { Check, Collection, Entity, Enum, Property, types } from '@mikro-orm/core';
 import { StakeholderCategory, StakeholderSegmentation } from '../../../../shared/domain/requirements/enums.js';
 import { ComponentModel, ComponentVersionsModel } from './ComponentSchema.js';
 import { ReqType } from "../../../../shared/domain/requirements/ReqType.js";
@@ -16,9 +16,11 @@ export class StakeholderVersionsModel extends ComponentVersionsModel {
     @Enum({ items: () => StakeholderCategory })
     readonly category!: StakeholderCategory
 
-    @Property({ type: types.integer, check: 'interest >= 0 AND interest <= 100' })
+    @Property({ type: types.integer })
+    @Check<StakeholderVersionsModel>({ expression: (cols) => `${cols.interest} >= 0 AND ${cols.interest} <= 100` })
     readonly interest!: number
 
-    @Property({ type: types.integer, check: 'influence >= 0 AND influence <= 100' })
+    @Property({ type: types.integer })
+    @Check<StakeholderVersionsModel>({ expression: (cols) => `${cols.influence} >= 0 AND ${cols.influence} <= 100` })
     readonly influence!: number
 }
