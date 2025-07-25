69bb054a3a9cfe2ff26da23a381d94d2a0a8d51f
Michael Haufe <tno@thenewobjective.com>
2025-07-25 16:44:43 +0000

    Enhance PegsLanding and system pages with additional disabled cards for better user guidance

--- components/PegsLanding.vue
+++ components/PegsLanding.vue
@@ -4,6 +4,7 @@ type Card = {
     icon: string
     label: string
     reqId: string
+    disabled?: boolean
 }
 
 const props = defineProps<{
@@ -17,27 +18,48 @@ const props = defineProps<{
     <slot name="header" />
 
     <section class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 text-center">
-        <NuxtLink
-            v-for="card in props.cards"
-            :key="card.name"
-            :to="{
-                name: card.name as any,
-                params: {
-                    solutionslug: props.solutionslug,
-                    organizationslug: props.organizationslug
-                }
-            }"
-        >
-            <UCard variant="subtle">
-                <template #header>
-                    <UIcon
-                        :name="card.icon"
-                        class="size-7"
-                    />
-                </template>
+        <template v-for="card in props.cards" :key="card.name">
+            <!-- Enabled card -->
+            <NuxtLink
+                v-if="!card.disabled"
+                :to="{
+                    name: card.name as any,
+                    params: {
+                        solutionslug: props.solutionslug,
+                        organizationslug: props.organizationslug
+                    }
+                }"
+            >
+                <UCard variant="subtle">
+                    <template #header>
+                        <UIcon
+                            :name="card.icon"
+                            class="size-7"
+                        />
+                    </template>
 
-                {{ card.reqId }}. {{ card.label }}
-            </UCard>
-        </NuxtLink>
+                    {{ card.reqId }}. {{ card.label }}
+                </UCard>
+            </NuxtLink>
+
+            <!-- Disabled card -->
+            <div v-else class="cursor-not-allowed">
+                <UCard variant="subtle" class="border-dashed border-gray-300 opacity-70">
+                    <template #header>
+                        <UIcon
+                            :name="card.icon"
+                            class="size-7 text-gray-400"
+                        />
+                    </template>
+
+                    <div>
+                        {{ card.reqId }}. {{ card.label }}
+                        <div class="text-red-500 text-sm mt-2 font-medium">
+                            Coming Soon
+                        </div>
+                    </div>
+                </UCard>
+            </div>
+        </template>
     </section>
 </template>
