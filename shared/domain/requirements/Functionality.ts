7baebb2fe22d0cdc6813698217a0be8e51b6b55b
Michael Haufe <tno@thenewobjective.com>
2025-08-11 19:30:26 +0000

    Refactor requirement models and behaviors to enhance functionality and constraints

--- shared/domain/requirements/Functionality.ts
+++ shared/domain/requirements/Functionality.ts
@@ -1,12 +1,19 @@
 import { z } from 'zod'
 import { Behavior } from './Behavior.js'
 import { ReqType } from './ReqType.js'
+import { dedent } from '../../../shared/utils/dedent.js'
 
 export const Functionality = Behavior.extend({
     reqId: z.string().regex(/^G\.4\.\d+$/, 'Format must be G.4.#').optional()
         .describe('The user-friendly identifier of the requirement that is unique within its parent'),
     reqIdPrefix: z.literal('G.4.').default('G.4.'),
     reqType: z.nativeEnum(ReqType).default(ReqType.FUNCTIONALITY)
-}).describe('Functionality describes at a high-level what behavior a system should exhibit. The details are described in the Systems section')
+}).describe(dedent(`
+    Functionality describes at a high-level what behavior a system should exhibit. 
+    The details are described in the Systems section.
+    Generally expressed in the form "The system shall provide <capability>."
+    
+    Example: "The system shall provide user authentication and authorization capabilities."
+`))
 
 export type FunctionalityType = z.infer<typeof Functionality>
--- shared/domain/requirements/NonFunctionalBehavior.ts
+++ shared/domain/requirements/NonFunctionalBehavior.ts
@@ -1,13 +1,18 @@
 import { z } from 'zod'
-import { Functionality } from './Functionality.js'
+import { Behavior } from './Behavior.js'
 import { dedent } from '../../../shared/utils/dedent.js'
 import { ReqType } from './ReqType.js'
 
-export const NonFunctionalBehavior = Functionality.extend({
+export const NonFunctionalBehavior = Behavior.extend({
     reqId: z.string().regex(/^S\.2\.2\.\d+$/, 'Format must be S.2.2.#').optional()
         .describe('The user-friendly identifier of the requirement that is unique within its parent'),
     reqIdPrefix: z.literal('S.2.2.').default('S.2.2.'),
-    reqType: z.nativeEnum(ReqType).default(ReqType.NON_FUNCTIONAL_BEHAVIOR)
+    reqType: z.nativeEnum(ReqType).default(ReqType.NON_FUNCTIONAL_BEHAVIOR),
+    functionality: z.object({
+        id: z.string().uuid().describe('The ID of the related functionality'),
+        name: z.string().describe('The name of the related functionality'),
+        reqType: z.nativeEnum(ReqType).default(ReqType.FUNCTIONALITY)
+    }).optional().describe('The related high-level functionality (G.4.#) that this behavior implements')
 }).describe(dedent(`
     NonFunctionalBehavior is a type of Behavior that is not directly related to the functionality of a system.
     It specifies **how** the system should behave, i.e., the qualities that the system must exhibit.
